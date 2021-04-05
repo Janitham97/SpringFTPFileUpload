fe084231066a110c1b1ef5a7fae81cdb0cc87a45
Janitha Missaka <janitha.missaka@gmail.com>
2021-04-05 22:33:40 +0530

    all done

---
+++ src/main/java/spirngftpuploadtest/demo/config/FTPConfig.java
@@ -0,0 +1,30 @@
+package spirngftpuploadtest.demo.config;
+
+import org.apache.commons.net.PrintCommandListener;
+import org.apache.commons.net.ftp.FTP;
+import org.apache.commons.net.ftp.FTPClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+@Configuration
+public class FTPConfig {
+    @Bean
+    public FTPClient client() throws IOException {
+        var ftp = new FTPClient();
+        ftp.addProtocolCommandListener(new PrintCommandListener(new PrintWriter(System.out)));
+        ftp.connect("localhost", 2121);
+        System.out.println(ftp.getReplyCode());
+        ftp.login("", ""); //enter your FTP server username and password here
+
+//        ftp.enterLocalPassiveMode(); // important!
+        ftp.setFileType(FTP.BINARY_FILE_TYPE);
+
+        return ftp;
+    }
+
+}
